54b0844b09abb86afa9dbd3f8b3e2bf54a0c297b
hubeixuegao@gamil.com <qq23612244896>
2022-04-05 17:15:27 +0800

    js

---
+++ FirstJs/js/example/silly_story_generator/main.js
@@ -0,0 +1,56 @@
+console.log("dadads");
+
+// 根据id获取元素
+const customerName = document.getElementById("customname");
+//  querySelector() 方法仅仅返回匹配指定选择器的第一个元素
+// 根据class获取元素
+const story = document.querySelector(".story");
+// 根据class获取元素
+const randomize = document.querySelector(".randomize");
+
+// 随机从数组中取值
+function randomValueFromArray(array) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+}
+
+let storyText = '今天气温 35 摄氏度，:insertx:出门散步。当走到:inserty:门前时，突然就:insertz:。人们都惊呆了，李雷全程目睹但并没有慌，因为:insertx:是一个 140 公斤的胖子，天气又辣么热。';
+let insertX = ['怪兽威利', '大老爹', '圣诞老人'];
+let insertY = ['肯德基', '迪士尼乐园', '白宫'];
+let insertZ = ['自燃了', '在人行道化成了一坨泥', '变成一只鼻涕虫爬走了'];
+
+console.log(randomize);
+// 为 button 中 class名称为 randomize的对象，添加一个监听器
+// 于是当所引用的按钮被点击时，result() 函数就会运行。
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+
+    if (customerName.value !== '') {
+        const name = customerName.value;
+        newStory = newStory.replace('李雷', name);
+    }
+
+    if (document.getElementById("american").checked) {
+        const weight = Math.round(140 * 2.20462) + ' 磅';
+        const temperature = Math.round(35 * 9 / 5 + 32) + ' 华氏度';
+        newStory = newStory.replace('35 摄氏度', temperature);
+        newStory = newStory.replace('140 公斤', weight);
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+
+
+    document.querySelector('html').style.backgroundColor = 'red';
+}
