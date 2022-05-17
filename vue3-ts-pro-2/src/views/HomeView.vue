561dc25ba6bc6e1f8f31f34911820cf2e1e777b3
fjm <Xuegaoyu3>
2022-05-17 20:18:44 +0800

    login

--- vue3-ts-pro-2/src/views/HomeView.vue
+++ vue3-ts-pro-2/src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to xuegao im" />
+    <router-link to="/login">login</router-link>
   </div>
 </template>
 
@@ -14,5 +15,5 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue { }
 </script>
---
+++ vue3-ts-pro-2/src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login">
+    <el-form :model="form" label-width="120px">
+      <el-form-item label="用户名">
+        <el-input v-model="form.username" placeholder="请入输入用户名" />
+      </el-form-item>
+
+      <el-form-item label="密码">
+        <el-input v-model="form.password" type="password" placeholder="请入输入密码" show-password />
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-button @click="onReset">重置</el-button>
+      </el-form-item>
+
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from 'vue'
+
+export default {
+  setup() {
+    const form = reactive({
+      username: '',
+      password: '',
+    })
+
+    const onSubmit = () => {
+      console.log("submit", form)
+    }
+    const onReset = () => {
+      form.username = ''
+      form.password = ''
+    }
+    return {
+      form, onSubmit, onReset
+    }
+  }
+}
+
+</script>
