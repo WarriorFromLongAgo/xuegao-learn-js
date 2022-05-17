f9043142f29115834ec5b594ce96ce0633dbcb93
fjm <Xuegaoyu3>
2022-05-17 14:31:01 +0800

    init

--- vue3-ts/src/router/index2.ts
+++ vue3-ts/src/router/index2.ts
@@ -1,29 +1,29 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+// import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+// import { createApp, h } from 'vue'
 
-// https://v3.cn.vuejs.org/guide/routing.html#%E4%BB%8E%E9%9B%B6%E5%BC%80%E5%A7%8B%E7%AE%80%E5%8D%95%E7%9A%84%E8%B7%AF%E7%94%B1
-const NotFoundComponent = { template: '<p>Page not found</p>' }
-const HomeComponent = { template: '<p>Home page</p>' }
-const AboutComponent = { template: '<p>About page</p>' }
+// const NotFoundComponent = { template: '<p>Page not found</p>' }
+// const HomeComponent = { template: '<p>Home page</p>' }
+// const AboutComponent = { template: '<p>About page</p>' }
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeComponent
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+// const routes = {
+//   '/': HomeComponent,
+//   '/about': AboutComponent
+// }
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+// const SimpleRouter = {
+//   data: () => ({
+//     currentRoute: window.location.pathname
+//   }),
 
-export default router
+//   computed: {
+//     CurrentComponent() {
+//       return routes[this.currentRoute] || NotFoundComponent
+//     }
+//   },
+
+//   render() {
+//     return h(this.CurrentComponent)
+//   }
+// }
+
+// createApp(SimpleRouter).mount('#app')
