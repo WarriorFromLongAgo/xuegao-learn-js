42b0f5b5995e37d4862d9d4403c287a12b086557
fjm <593436@ky-tech.com.cn>
2022-05-27 23:32:46 +0800

    el loading

--- vue3-ts-pro-2/src/views/TestView/TestView1.vue
+++ vue3-ts-pro-2/src/views/TestView/TestView1.vue
@@ -1,19 +1,53 @@
 <template>
   <div>
     <input type="button" @click="alterMsg" value="1111">
+    <input type="button" @click="elLoading" value="1111">
+    <input type="button" @click="elLoadingByQuery" value="1111">
   </div>
 </template>
 <script lang="ts">
-import { ElMessage } from 'element-plus'
+import { ElLoading, ElMessage } from 'element-plus'
 
 export default {
   setup() {
     const alterMsg = (msg: string) => {
       ElMessage.warning("asaa = " + msg)
     }
+    const elLoading = () => {
+      const loadingInstance = ElLoading.service({
+        lock: true,
+        text: '转一转',
+        background: 'rgba(0, 0, 0, 0.1)'
+      })
+      let count = 0;
+      setTimeout(() => {
+        count++;
+        console.log("count = ", count);
+        if (count > 3) {
+          loadingInstance.close()
+        }
+      }, 1000)
+    }
+    const elLoadingByQuery = () => {
+      let loadingInstance
+      try {
+        loadingInstance = ElLoading.service({
+          lock: true,
+          text: '转一转',
+          background: 'rgba(0, 0, 0, 0.1)'
+        })
+        // await queryXXXX(params)
+        setTimeout(() => {
+          loadingInstance.close()
+        }, 3000)
+      } catch (err) {
+        console.log(err);
+        throw (err)
+      }
+    }
 
     return {
-      alterMsg
+      alterMsg, elLoading, elLoadingByQuery
     }
   }
 }
