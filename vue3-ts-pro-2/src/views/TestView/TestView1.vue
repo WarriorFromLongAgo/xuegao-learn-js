7dfb96346ddaae4388e75638729ee771e826844f
xuegao <hubeixuegao@gamil.com>
2022-05-28 13:04:16 +0800

    vue3

--- vue3-ts-pro-2/src/views/TestView/TestView1.vue
+++ vue3-ts-pro-2/src/views/TestView/TestView1.vue
@@ -5,12 +5,14 @@
     <input type="button" @click="elLoadingByQuery" value="1111">
     <input type="button" @click="getLoading" value="1111">
     <input type="button" @click="elLoadingByQueryV2" value="1111">
+    <input type="button" @click="elLoadingByQueryError" value="1111">
   </div>
 </template>
 <script lang="ts">
 import { ElLoading, ElMessage } from 'element-plus'
 import { openLoading, closeLoading } from "@/utils/loading"
 import { LoadingInstance } from 'element-plus/lib/components/loading/src/loading'
+import { ref } from 'vue'
 
 export default {
   setup() {
@@ -62,9 +64,33 @@ export default {
       }, 3000)
     }
 
+    let refCount = ref(0);
+    const elLoadingByQueryError = () => {
+      // 请忽视下面的两行
+      let loadingInstance: LoadingInstance;
+      loadingInstance = ElLoading.service({
+        lock: true,
+        text: '转一转',
+        background: 'rgba(0, 0, 0, 0.1)'
+      })
+      try {
+        refCount.value++;
+        console.log(refCount.value);
+        if (refCount.value == 3) {
+          throw new Error('Whoops!');
+        }
+        setTimeout(() => {
+          loadingInstance.close()
+        }, 1000)
+      } catch (e: unknown) {
+        loadingInstance.close();
+        console.log("catch", e);
+      }
+    }
+
 
     return {
-      alterMsg, elLoading, elLoadingByQuery, getLoading, elLoadingByQueryV2
+      alterMsg, elLoading, elLoadingByQuery, getLoading, elLoadingByQueryV2, elLoadingByQueryError
     }
   }
 }
