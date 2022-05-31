1c1f3d848c0fb58163eef99881cf45a39d196c14
fjm <593436@ky-tech.com.cn>
2022-05-31 13:17:19 +0800

    vue3

--- vue3-ts-pro-2/src/request/http/axios5/testHttp.ts
+++ vue3-ts-pro-2/src/request/http/axios5/testHttp.ts
@@ -1,16 +1,14 @@
 import { IResponse, ILogin } from './type';
 import { axiosInstance } from './api';
 
-const API_URL = "/api"
-
 export const getTest1 = (): Promise<IResponse> => {
   console.log('getTest1 ====');
-  return axiosInstance.get(API_URL + '/vue/get1').then(res => res.data);
+  return axiosInstance.get('/vue/get1').then(res => res.data);
 };
 
 export const postTest1 = (params: ILogin): Promise<IResponse> => {
   console.log('postTest1 ====', params);
-  return axiosInstance.post(API_URL + '/vue/post1', params).then(res => res.data);
+  return axiosInstance.post('/vue/post1', params).then(res => res.data);
 };
 
 
@@ -21,7 +19,7 @@ export const postTest1 = (params: ILogin): Promise<IResponse> => {
  */
  export const login = (params: ILogin): Promise<IResponse> => {
   console.log('login ====', params);
-  return axiosInstance.post(API_URL + '/user/login', params).then(res => res.data);
+  return axiosInstance.post('/user/login', params).then(res => res.data);
 };
 
 /**
@@ -31,6 +29,6 @@ export const postTest1 = (params: ILogin): Promise<IResponse> => {
  */
 export const loginAsync = async (params: ILogin): Promise<IResponse> => {
   console.log('loginAsync ========', params);
-  const res = await axiosInstance.post(API_URL + '/user/login', params);
+  const res = await axiosInstance.post('/user/login', params);
   return res.data;
 };
--- vue3-ts-pro-2/vue.config.js
+++ vue3-ts-pro-2/vue.config.js
@@ -11,15 +11,15 @@ module.exports = defineConfig({
     https: false,
     open: true,
     //反向代理配置，注意rewrite写法，开始没看文档在这里踩了坑
-    proxy: {
-      '/api': {
-        // 接口域名（你请求的第三方接口）
-        target: 'http://127.0.0.1:8000',
-        // 是否跨域 （虚拟的站点需要更管origin）
-        changeOrigin: true,
-        // 路径重置
-        pathRewrite: { '^/api': '' }
-      }
-    }
+    // proxy: {
+      // '/api': {
+      //   // 接口域名（你请求的第三方接口）
+      //   target: 'http://127.0.0.1:8000',
+      //   // 是否跨域 （虚拟的站点需要更管origin）
+      //   changeOrigin: true,
+      //   // 路径重置
+      //   pathRewrite: { '^/api': '' }
+      // }
+    // }
   },
 })
